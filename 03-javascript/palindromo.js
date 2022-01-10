aa6dcc3da3d2cd479ba3b19c6b0fba1e08a0f3c8
WarleyLeandro <warley.leandro95@gmail.com>
2022-01-10 17:22:21 -0300

    exercicio pares to zero

--- 03-javascript/palindromo.js
+++ 03-javascript/palindromo.js
@@ -1,17 +1,17 @@
 const palindromo = (string) => {
-  if (!string) return;
-  return string.split("").reverse().join("") == string
+    if (!string) return;
+    return string.split("").reverse().join("") == string
 };
 
 console.log(palindromo("arara"))
 console.log(palindromo("casa"))
 
 const palindromo2 = (string) => {
-  if (!string) return;
-  for (let i = 0; i < string.length / 2; i++) {
-      if(string[i] !== string[string.length -1 - i]) return false
-  }
-  return true
+    if (!string) return;
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) return false
+    }
+    return true
 };
 
 console.log(palindromo2("arara"))
