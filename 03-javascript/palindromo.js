ca4ebd921d2cc649e2d22c259d3114c9747b26e9
WarleyLeandro <warley.leandro95@gmail.com>
2022-01-10 17:09:42 -0300

    exercicio js palindromo

---
+++ 03-javascript/palindromo.js
@@ -0,0 +1,18 @@
+const palindromo = (string) => {
+  if (!string) return;
+  return string.split("").reverse().join("") == string
+};
+
+console.log(palindromo("arara"))
+console.log(palindromo("casa"))
+
+const palindromo2 = (string) => {
+  if (!string) return;
+  for (let i = 0; i < string.length / 2; i++) {
+      if(string[i] !== string[string.length -1 - i]) return false
+  }
+  return true
+};
+
+console.log(palindromo2("arara"))
+console.log(palindromo2("casa"))
